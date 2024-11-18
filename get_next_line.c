5b1e1deb863988bbb41bbcab6e91cfade9acfb56
duyt1713 <tungha1310@gmail.com>
2024-11-18 16:55:14 +0200

    backup 18.11

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,19 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 08:04:31 by duha              #+#    #+#             */
-/*   Updated: 2024/11/14 08:07:19 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/18 16:54:35 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 char	*get_next_line(int fd)
 {
-	
+	char	*p;
+
+	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE <= 0)
+		return (NULL);
+	return (p);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,27 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 08:04:29 by duha              #+#    #+#             */
-/*   Updated: 2024/11/14 08:06:46 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/18 16:51:05 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+/**
+ * get_next_line - Returns a line read from a file descriptor.
+ *
+ * @fd: The file descriptor to read from.
+ *
+ * External functions: read, malloc, free
+ * 
+ * Return: Read line: correct behavior
+ *         or NULL if there is nothing else to read, or an error occurred.
+ */
 char	*get_next_line(int fd);
 
 #endif
