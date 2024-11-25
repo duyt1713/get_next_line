31e68d891248dc32f57f4097bfcd49d91c4365d3
duyt1713 <tungha1310@gmail.com>
2024-11-26 01:34:58 +0200

    backup 26.11

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 08:04:31 by duha              #+#    #+#             */
-/*   Updated: 2024/11/18 16:54:35 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/25 21:01:57 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*get_next_line(int fd)
 {
 	char	*p;
 
-	if (fd < 0 || fd >= OPEN_MAX || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	return (p);
 }
