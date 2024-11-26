bd1c2d0713c8d2b4cd45bfa078264865c788f5bb
duyt1713 <tungha1310@gmail.com>
2024-11-26 03:05:32 +0200

    backup 26.11

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 08:04:31 by duha              #+#    #+#             */
-/*   Updated: 2024/11/25 21:01:57 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/26 02:45:53 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * get_next_line - Returns a line read from a file descriptor.
+ *
+ * @fd: The file descriptor to read from.
+ *
+ * External functions: read, malloc, free
+ *
+ * Return: Read line: correct behavior
+ *         or NULL if there is nothing else to read, or an error occurred.
+ */
 char	*get_next_line(int fd)
 {
 	char	*p;
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: duha <duha@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 08:04:29 by duha              #+#    #+#             */
-/*   Updated: 2024/11/18 16:51:05 by duha             ###   ########.fr       */
+/*   Updated: 2024/11/26 02:45:47 by duha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,6 @@
 #  define BUFFER_SIZE 42
 # endif
 
-/**
- * get_next_line - Returns a line read from a file descriptor.
- *
- * @fd: The file descriptor to read from.
- *
- * External functions: read, malloc, free
- * 
- * Return: Read line: correct behavior
- *         or NULL if there is nothing else to read, or an error occurred.
- */
 char	*get_next_line(int fd);
 
 #endif
