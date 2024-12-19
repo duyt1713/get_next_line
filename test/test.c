8fcb473db5c0aaa5e33ea4d955894a56d79bc08c
duyt1713 <tungha1310@gmail.com>
2024-12-19 07:22:28 +0200

    backup 19.12

--- test/test.c
+++ test/test.c
@@ -2,18 +2,23 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(void)
 {
 	int	fd;
+	ssize_t bytes;
 	const char *files[] = {"one_line.txt", "two_lines.txt", "lots_of_lines.txt", "non_exsistence.txt"};
 	int num_files = sizeof(files) / sizeof(files[0]);
-	int i = 0;
 
-	while (i < num_files)
+	for (int i = 0; i < num_files; i++)
 	{
 		fd = open(files[i], O_RDONLY);
 		printf("Opening %s: %d\n", files[i], fd);
-		i++;
+		char *buffer = malloc(100);
+		bytes = read(fd, buffer, 20);
+		printf("Read: %s\n", buffer);
+		free(buffer);
 	}
 }
